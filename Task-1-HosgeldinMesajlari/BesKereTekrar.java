eed37ef220815ba68cd1e6f59dd1acb15988bea7
UtkuEfeGursoy <250542005@firat.edu.tr>
2025-10-21 17:14:01 +0300

    Add BesKereTekrar.java to print welcome message

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class BesKereTekrar {
+    public static void main(String[] args) throws Exception {
+       System.out.println("Java'ya Hoş Geldiniz");
+         System.out.println("Java'ya Hoş Geldiniz");
+           System.out.println("Java'ya Hoş Geldiniz");
+             System.out.println("Java'ya Hoş Geldiniz");
+               System.out.println("Java'ya Hoş Geldiniz");
+    }
+}
