e728326fd2924b80be046a24f104d95cf96734a7
UtkuEfeGursoy <250542005@firat.edu.tr>
2025-10-21 17:57:47 +0300

    Add ToplamaSerisi class to sum numbers from 1 to 9
    Calculates the sum of numbers from 1 to 9 and prints the result.

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,9 @@
+public class ToplamaSerisi {
+    public static void main(String[] args) throws Exception {
+        //Sonucu Hesaplama
+        int i=1+2+3+4+5+6+7+8+9;
+
+        //Sonucu Yazdırma
+        System.out.println("1'den 9'a kadar olan sayıların toplamı: "+i);
+    }
+}
