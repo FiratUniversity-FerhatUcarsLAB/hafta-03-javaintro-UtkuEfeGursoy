de855bda20bd402f15742a69ca79d0d9b135f9d1
UtkuEfeGursoy <250542005@firat.edu.tr>
2025-10-21 17:06:25 +0300

    Add UcMesaj class with welcome messages

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+public class UcMesaj {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Java'ya Hoş Geldiniz!");
+        System.out.println("Bilgisiyar Bilimlerine Hoş Geldiniz!");
+        System.out.println("Proglamlama Eğlencelidir!");
+
+    }
+}
