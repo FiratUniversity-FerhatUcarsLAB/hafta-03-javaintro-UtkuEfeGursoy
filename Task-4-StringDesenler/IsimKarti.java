4dc038c572aa1dd758813a97d3b1cf64b70a01ae
UtkuEfeGursoy <250542005@firat.edu.tr>
2025-10-23 21:12:45 +0300

    Add İsimKarti class to display personal information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,15 @@
+public class İsimKarti {
+    public static void main(String[] args) {
+        System.out.println(" *************************");
+        System.out.println(" # #");
+        System.out.println(" # İsminiz: Utku Efe ");
+        System.out.println(" # Soyisminiz: Gürsoy");
+        System.out.println(" # Yaşınız: 18 ");
+        System.out.println(" # Mesleğiniz: Öğrenci ");
+        System.out.println(" # Şehriniz: Elazığ ");
+        System.out.println(" # Öğrenci No: 250542005 ");
+        System.out.println(" # #");
+        System.out.println(" *************************");
+    }
+
+}
